bead2d795e63a76d70107153b5a61abe58680204
mikaeltreppo <mikaeltreppo@outlook.com>
2023-03-08 17:07:36 +0100

    corretto

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const members = [
         "name": "Wayne Barnett", "position": "Founder & CEO", "profile": "wayne-barnett-founder-ceo.jpg"
     } ,
     {
-        "name": "Angela Caroll", "position": "Chief Editor ", "profile": "  angela-caroll-chief-editor.jpg"
+        "name": "Angela Caroll", "position": "Chief Editor ", "profile": " angela-caroll-chief-editor.jpg"
     } ,
     {
         "name": "Walter Gordon", "position": "Office Manager ", "profile": "walter-gordon-office-manager.jpg"
@@ -17,28 +17,27 @@ const members = [
         "name": "Angela Lopez", "position": "Social Media Manager", "profile": "angela-lopez-social-media-manager.jpg"
     } ,
     {
-        "name": "Scott Estrada", "position": "Developer", "profile": " scott-estrada-developer.jpg"
+        "name": "Scott Estrada", "position": "Developer", "profile": "scott-estrada-developer.jpg"
     } ,
     {
         "name": "Barbara Ramos", "position": "Graphic Designer", "profile": "barbara-ramos-graphic-designer.jpg"
     }
 ]
-let position;
-let profile;
+
 function esport (lenghtArray){
     for(let i = 0; i<lenghtArray; i++){
-    let name = members[i].name;
-    let position = members[i].position;
-    let profile = members[i].profile;
-    printable.push(name);   printable.push(position);    printable.push(profile);
+    let name = members[i].name ;
+    let position = members[i].position ;
+    let profile = members[i].profile ;
+    membersDom.innerHTML += "<div><h2>" + name + "</h2><p>" + position + "</p><img src='" + profile + "' alt='" + name + "'></div>";;
 }}
 
+
+
+
+
+
 esport(members.length)
-
-membersDom.innerHTML= printable;
-
-
-
 
 
 
