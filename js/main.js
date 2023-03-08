6420745de1e9885f50491358ad021ea419b87564
mikaeltreppo <mikaeltreppo@outlook.com>
2023-03-08 15:48:09 +0100

    oggetti aggiunti

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const members = [
+    {
+        "name": "Wayne Barnett", "position": "Founder & CEO", "profile": "wayne-barnett-founder-ceo.jpg"
+    } ,
+    {
+        "name": "Angela Caroll", "position": "Chief Editor ", "profile": "  angela-caroll-chief-editor.jpg"
+    } ,
+    {
+        "name": "Walter Gordon", "position": "Office Manager ", "profile": "walter-gordon-office-manager.jpg"
+    } ,
+    {
+        "name": "Angela Lopez", "position": "Social Media Manager", "profile": "angela-lopez-social-media-manager.jpg"
+    } ,
+    {
+        "name": "Scott Estrada", "position": "Developer", "profile": " scott-estrada-developer.jpg"
+    } ,
+    {
+        "name": "Barbara Ramos", "position": "Graphic Designer", "profile": "barbara-ramos-graphic-designer.jpg"
+    }
+]
+
+
+console.log(members);
+
+
+
+
+
+
+
